14a5d8aa9bb28fbae4cd531fdd98c83ea42bccb0
Desktop-Mono <nzubiria1@gmail.com>
2018-02-15 12:01:16 -0500

    AimAt delegation from tank to AimComponent, starting speed, etc.

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
@@ -16,17 +17,27 @@ public:
 	ATank();
 
 protected:
+	
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
+private:
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-private:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(EditAnywhere, Category = Firing)
+	float LaunchSpeed = 100000;
+
 public:
+
 	void AimAt(FVector HitLocation);
 	
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
 };
